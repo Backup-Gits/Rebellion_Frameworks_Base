46809a468b3c0ca96fd82158c6c3510723686677
Nico60 <ndhouailly@gmail.com>
2018-04-11 21:03:36 +0200

    base: QS panel opacity [1/2]
    Change-Id: Ieabb916f065a91be0cacbc88fe0028d0ffc19f56
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5447,6 +5447,12 @@ public final class Settings {
         private static final Validator LOCKSCREEN_WEATHER_SHOW_IMAGE_VALIDATOR = BOOLEAN_VALIDATOR;
 
         /**
+         * Whether to change the transparency of the qs panel
+         * @hide
+         */
+        public static final String QS_PANEL_BG_ALPHA = "qs_panel_bg_alpha";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
