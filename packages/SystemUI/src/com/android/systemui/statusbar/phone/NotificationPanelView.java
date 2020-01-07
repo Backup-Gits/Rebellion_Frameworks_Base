b7d0e11b798fefe58a78a17f1d06c30263742614
00day0 <therandomuser11@gmail.com>
2020-01-07 19:44:06 +0100

    OxygenOS dismiss all: animate it todo: cleanup of all this mess
    instead of all this mess with scaling animation lets just switch opacity based on how much panel is expanded
    animation gets in sync with future qs blur animation + it doesnt rely on these damnn checks if panel is fully expanded etc etc etc
    
    Change-Id: I5b29358996e04016c27cab6851f1d8f0b209f088
    Signed-off-by: 00day0 <therandomuser11@gmail.com>
    Signed-off-by: rohan <purohit.rohan@gmail.com>
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -1270,7 +1270,7 @@ public class NotificationPanelView extends PanelView implements
         return !mQsTouchAboveFalsingThreshold;
     }
 
-    private float getQsExpansionFraction() {
+    public float getQsExpansionFraction() {
         return Math.min(1f, (mQsExpansionHeight - mQsMinExpansionHeight)
                 / (mQsMaxExpansionHeight - mQsMinExpansionHeight));
     }
@@ -2310,6 +2310,7 @@ public class NotificationPanelView extends PanelView implements
         }
         mNotificationStackScroller.setAlpha(alpha);
         mStatusBar.updateBlurVisibility();
+        mStatusBar.setDismissAllVisible(true);
     }
 
     private float getFadeoutAlpha() {
