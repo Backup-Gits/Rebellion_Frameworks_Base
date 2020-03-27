2f05682093b2b4fdb47acdfb8f22e47c1c40327b
SagarMakhar <sagarmakhar@gmail.com>
2020-03-27 12:48:55 +0000

    QsFooter: Animate icons while showing/hiding
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Change-Id: Ie80937713be75040edfa046d7d9ae240aa668833
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
@@ -220,7 +220,9 @@ public class QSFooterImpl extends FrameLayout implements QSFooter,
     @Nullable
     private TouchAnimator createFooterAnimator() {
         return new TouchAnimator.Builder()
-                .addFloat(mActionsContainer, "alpha", 1, 1) // contains mRunningServicesButton
+                .addFloat(mSettingsContainer, "alpha", isSettingButtonEnabled() ? 1 : 0, 1)
+                .addFloat(mMultiUserAvatar, "alpha", 0, 1)
+                .addFloat(mRunningServicesButton, "alpha", 0, 1)
                 .addFloat(mEditContainer, "alpha", 0, 1)
                 .addFloat(mDragHandle, "alpha", 0, 0, 0)
                 .addFloat(mPageIndicator, "alpha", 0, 1)
