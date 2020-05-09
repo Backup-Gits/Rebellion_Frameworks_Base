Project-Xtended/frameworks_base

Base: Introduce optional Intelligent Profiles
if you use spectrum profiles and have the following prop: perist.spectrum.profile u can enable this feature with the following config: config_intelligent_performance_profile

What is this feature doing ?

Depending on what you are doing different spectrum profiles will be triggert. For example gaming will start performance profile, while enabling battery saver or turning screen off will enable powersave profile to reduce cpu, gpu freq and so on.  This allows us to have awesome performance on games and saving massive battery on screen off and battery saver mode.
When u turn off screen your profile will be switched to screen off profile in my case.
Attention here: Using to lower freq may make your fingerprint reader and unlock lower. Best thing todo is to create a mixed profile with balanced and powersave.

config_balanced_profile - default profile
config_performance_profile - is used when a game is started
config_powersave_profile - is used when battery saving is enabled
config_screen_off_profile - is used when screen goes off

Important for Gaming Boost: Gaming mode should be available on Rom! There is no gaming detection implemented!

Author: @ZeNiXxX

Following commits are needed for sepolicy big thx to @SuperDroidBond:

For enforcing:
https://github.com/Havoc-OS/android_system_sepolicy/commit/a63c1c357abd4953106b927fa83c155c90de7290

https://github.com/Havoc-OS/android_device_havoc_sepolicy/commit/5f05e13c758debdbb49ec9c751223a11cf5b58bd

core/java/com/android/internal/util/gaming/GamingModeController.java
@@ -44,6 +44,7 @@ import android.os.RemoteException;
 import android.os.Process;
 import android.os.UserHandle;
 import android.text.TextUtils;
+import android.os.SystemProperties;
 
 import java.util.Arrays;
 import java.util.ArrayList;
@@ -51,6 +52,7 @@ import java.util.List;
 
 import com.android.internal.R;
 import com.android.internal.notification.SystemNotificationChannels;
+import com.android.internal.util.xtended.XtendedUtils;
 
 import android.provider.Settings;
 import android.provider.Settings.Global;
@@ -70,6 +72,7 @@ public class GamingModeController {
     private ArrayList<String> mGameApp = new ArrayList<String>();
 
     private boolean mGamingModeActivated;
+    private String mCurrentPerformanceProfileState;
     private static int mRingerState;
     private static int mZenState;
     private static int mHwKeysState;
@@ -80,6 +83,8 @@ public class GamingModeController {
 
     private static final String TAG = "GamingModeController";
     private static final int GAMING_NOTIFICATION_ID = 420;
+
+    public static final String SPECTRUM_SYSTEM_PROPERTY = "persist.spectrum.profile";
 
     public GamingModeController(Context context) {
         mContext = context;
@@ -347,9 +352,24 @@ public class GamingModeController {
             return;
         mGamingModeActivated = enabled && mGamingModeEnabled;
         if (mGamingModeActivated) {
+            if(XtendedUtils.IntelligentPerformanceProfileAvailable()){
+                deviceSpecificPerfermanceModeHandler(mGamingModeActivated);
+            }
             enableGamingFeatures();
         } else {
+            if(XtendedUtils.IntelligentPerformanceProfileAvailable()) {
+                deviceSpecificPerfermanceModeHandler(mGamingModeActivated);
+            }
             disableGamingFeatures();
+        }
+    }
+
+    private void deviceSpecificPerfermanceModeHandler(boolean enabled) {
+        if(enabled) {
+            XtendedUtils.SetLastPerformanceProfileToSettings(mContext, SystemProperties.get(SPECTRUM_SYSTEM_PROPERTY, Resources.getSystem().getString(R.string.config_balanced_profile)));
+            SystemProperties.set(SPECTRUM_SYSTEM_PROPERTY, Resources.getSystem().getString(R.string.config_performance_profile));
+        } else {
+                SystemProperties.set(SPECTRUM_SYSTEM_PROPERTY, XtendedUtils.GetLastPerformanceProfileFromSettings(mContext));
         }
     }
 

packages/SystemUI/src/com/android/systemui/doze/DozeService.java
@@ -19,6 +19,8 @@ package com.android.systemui.doze;
 import android.content.Context;
 import android.os.PowerManager;
 import android.os.SystemClock;
+import android.os.SystemProperties;
+
 import android.service.dreams.DreamService;
 import android.util.Log;
 
@@ -29,6 +31,11 @@ import com.android.systemui.plugins.FalsingManager;
 import com.android.systemui.plugins.PluginListener;
 import com.android.systemui.shared.plugins.PluginManager;
 
+import com.android.internal.R;
+import com.android.internal.util.xtended.XtendedUtils;
+import android.content.res.Resources;
+import android.provider.Settings;
+
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
 
@@ -38,6 +45,8 @@ public class DozeService extends DreamService
         implements DozeMachine.Service, RequestDoze, PluginListener<DozeServicePlugin> {
     private static final String TAG = "DozeService";
     static final boolean DEBUG = Log.isLoggable(TAG, Log.DEBUG);
+
+    public static final String SPECTRUM_SYSTEM_PROPERTY = "persist.spectrum.profile";
 
     private DozeMachine mDozeMachine;
     private DozeServicePlugin mDozePlugin;
@@ -95,7 +104,10 @@ public class DozeService extends DreamService
         if (mDozePlugin != null) {
             mDozePlugin.onDreamingStarted();
         }
+        deviceSpecificPerfermanceModeHandler(true);
     }
+
+
 
     @Override
     public void onDreamingStopped() {
@@ -104,6 +116,23 @@ public class DozeService extends DreamService
         if (mDozePlugin != null) {
             mDozePlugin.onDreamingStopped();
         }
+        deviceSpecificPerfermanceModeHandler(false);
+    }
+
+    private void deviceSpecificPerfermanceModeHandler(boolean enabled) {
+        if(getUltraPowerSavingMode() && XtendedUtils.IntelligentPerformanceProfileAvailable()) {
+            if(enabled) {
+                XtendedUtils.SetLastPerformanceProfileToSettings(this, SystemProperties.get(SPECTRUM_SYSTEM_PROPERTY, Resources.getSystem().getString(R.string.config_balanced_profile)));
+                SystemProperties.set(SPECTRUM_SYSTEM_PROPERTY, Resources.getSystem().getString(R.string.config_screen_off_profile));
+            } else {
+                SystemProperties.set(SPECTRUM_SYSTEM_PROPERTY, XtendedUtils.GetLastPerformanceProfileFromSettings(this));
+            }
+        }
+    }
+
+    private boolean getUltraPowerSavingMode() {
+        return Settings.System.getInt(this.getContentResolver(),
+                Settings.System.ULTRA_POWER_SAVE, 1) != 0;
     }
 
     @Override

packages/SystemUI/src/com/android/systemui/statusbar/policy/BatteryControllerImpl.java
@@ -20,15 +20,19 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.Resources;
 import android.os.BatteryManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.PowerManager;
 import android.os.PowerSaveState;
 import android.util.Log;
+import android.os.SystemProperties;
 
 import androidx.annotation.Nullable;
 
+import com.android.internal.R;
+import com.android.internal.util.xtended.XtendedUtils;
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.settingslib.fuelgauge.BatterySaverUtils;
 import com.android.settingslib.fuelgauge.Estimate;
@@ -53,6 +57,7 @@ public class BatteryControllerImpl extends BroadcastReceiver implements BatteryC
     private static final String TAG = "BatteryController";
 
     public static final String ACTION_LEVEL_TEST = "com.android.systemui.BATTERY_LEVEL_TEST";
+    public static final String SPECTRUM_SYSTEM_PROPERTY = "persist.spectrum.profile";
 
     private static final boolean DEBUG = Log.isLoggable(TAG, Log.DEBUG);
     private static final int UPDATE_GRANULARITY_MSEC = 1000 * 60;
@@ -115,6 +120,16 @@ public class BatteryControllerImpl extends BroadcastReceiver implements BatteryC
     @Override
     public void setPowerSaveMode(boolean powerSave) {
         BatterySaverUtils.setPowerSaveMode(mContext, powerSave, /*needFirstTimeWarning*/ true);
+    }
+
+
+    private void deviceSpecificPerfermanceModeHandler(boolean enabled) {
+        if(enabled && XtendedUtils.IntelligentPerformanceProfileAvailable()) {
+            XtendedUtils.SetLastPerformanceProfileToSettings(mContext, SystemProperties.get(SPECTRUM_SYSTEM_PROPERTY, Resources.getSystem().getString(R.string.config_balanced_profile)));
+            SystemProperties.set(SPECTRUM_SYSTEM_PROPERTY, Resources.getSystem().getString(R.string.config_powersave_profile));
+        } else {
+            SystemProperties.set(SPECTRUM_SYSTEM_PROPERTY, XtendedUtils.GetLastPerformanceProfileFromSettings(mContext));
+        }
     }
 
     @Override
@@ -273,6 +288,12 @@ public class BatteryControllerImpl extends BroadcastReceiver implements BatteryC
     private void setPowerSave(boolean powerSave) {
         if (powerSave == mPowerSave) return;
         mPowerSave = powerSave;
+
+        if(powerSave) {
+            deviceSpecificPerfermanceModeHandler(true);
+        } else {
+            deviceSpecificPerfermanceModeHandler(false);
+        }
 
         // AOD power saving setting might be different from PowerManager power saving mode.
         PowerSaveState state = mPowerManager.getPowerSaveState(PowerManager.ServiceType.AOD);

Backup-Gits/Rebellion_Frameworks_Base

Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

core/java/com/android/internal/util/gaming/GamingModeController.java
@@ -44,6 +44,7 @@ import android.os.RemoteException;
 import android.os.Process;
 import android.os.UserHandle;
 import android.text.TextUtils;
+import android.os.SystemProperties;
 
 import java.util.Arrays;
 import java.util.ArrayList;
@@ -51,6 +52,7 @@ import java.util.List;
 
 import com.android.internal.R;
 import com.android.internal.notification.SystemNotificationChannels;
+import com.android.internal.util.rebellion.Utils;
 
 import android.provider.Settings;
 import android.provider.Settings.Global;
@@ -70,6 +72,7 @@ public class GamingModeController {
     private ArrayList<String> mGameApp = new ArrayList<String>();
 
     private boolean mGamingModeActivated;
+    private String mCurrentPerformanceProfileState;
     private static int mRingerState;
     private static int mZenState;
     private static int mHwKeysState;
@@ -80,6 +83,8 @@ public class GamingModeController {
 
     private static final String TAG = "GamingModeController";
     private static final int GAMING_NOTIFICATION_ID = 420;
+
+    public static final String SPECTRUM_SYSTEM_PROPERTY = "persist.spectrum.profile";
 
     public GamingModeController(Context context) {
         mContext = context;
@@ -347,9 +352,24 @@ public class GamingModeController {
             return;
         mGamingModeActivated = enabled && mGamingModeEnabled;
         if (mGamingModeActivated) {
+            if(Utils.IntelligentPerformanceProfileAvailable()){
+                deviceSpecificPerfermanceModeHandler(mGamingModeActivated);
+            }
             enableGamingFeatures();
         } else {
+            if(Utils.IntelligentPerformanceProfileAvailable()) {
+                deviceSpecificPerfermanceModeHandler(mGamingModeActivated);
+            }
             disableGamingFeatures();
+        }
+    }
+
+    private void deviceSpecificPerfermanceModeHandler(boolean enabled) {
+        if(enabled) {
+            Utils.SetLastPerformanceProfileToSettings(mContext, SystemProperties.get(SPECTRUM_SYSTEM_PROPERTY, Resources.getSystem().getString(R.string.config_balanced_profile)));
+            SystemProperties.set(SPECTRUM_SYSTEM_PROPERTY, Resources.getSystem().getString(R.string.config_performance_profile));
+        } else {
+                SystemProperties.set(SPECTRUM_SYSTEM_PROPERTY, Utils.GetLastPerformanceProfileFromSettings(mContext));
         }
     }
 

packages/SystemUI/src/com/android/systemui/doze/DozeService.java
@@ -19,6 +19,8 @@ package com.android.systemui.doze;
 import android.content.Context;
 import android.os.PowerManager;
 import android.os.SystemClock;
+import android.os.SystemProperties;
+
 import android.service.dreams.DreamService;
 import android.util.Log;
 
@@ -29,6 +31,11 @@ import com.android.systemui.plugins.FalsingManager;
 import com.android.systemui.plugins.PluginListener;
 import com.android.systemui.shared.plugins.PluginManager;
 
+import com.android.internal.R;
+import com.android.internal.util.rebellion.Utils;
+import android.content.res.Resources;
+import android.provider.Settings;
+
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
 
@@ -38,6 +45,8 @@ public class DozeService extends DreamService
         implements DozeMachine.Service, RequestDoze, PluginListener<DozeServicePlugin> {
     private static final String TAG = "DozeService";
     static final boolean DEBUG = Log.isLoggable(TAG, Log.DEBUG);
+
+    public static final String SPECTRUM_SYSTEM_PROPERTY = "persist.spectrum.profile";
 
     private DozeMachine mDozeMachine;
     private DozeServicePlugin mDozePlugin;
@@ -95,7 +104,10 @@ public class DozeService extends DreamService
         if (mDozePlugin != null) {
             mDozePlugin.onDreamingStarted();
         }
+        deviceSpecificPerfermanceModeHandler(true);
     }
+
+
 
     @Override
     public void onDreamingStopped() {
@@ -104,6 +116,23 @@ public class DozeService extends DreamService
         if (mDozePlugin != null) {
             mDozePlugin.onDreamingStopped();
         }
+        deviceSpecificPerfermanceModeHandler(false);
+    }
+
+    private void deviceSpecificPerfermanceModeHandler(boolean enabled) {
+        if(getUltraPowerSavingMode() && Utils.IntelligentPerformanceProfileAvailable()) {
+            if(enabled) {
+                Utils.SetLastPerformanceProfileToSettings(this, SystemProperties.get(SPECTRUM_SYSTEM_PROPERTY, Resources.getSystem().getString(R.string.config_balanced_profile)));
+                SystemProperties.set(SPECTRUM_SYSTEM_PROPERTY, Resources.getSystem().getString(R.string.config_screen_off_profile));
+            } else {
+                SystemProperties.set(SPECTRUM_SYSTEM_PROPERTY, Utils.GetLastPerformanceProfileFromSettings(this));
+            }
+        }
+    }
+
+    private boolean getUltraPowerSavingMode() {
+        return Settings.System.getInt(this.getContentResolver(),
+                Settings.System.ULTRA_POWER_SAVE, 1) != 0;
     }
 
     @Override

packages/SystemUI/src/com/android/systemui/statusbar/policy/BatteryControllerImpl.java
@@ -20,15 +20,19 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.res.Resources;
 import android.os.BatteryManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.PowerManager;
 import android.os.PowerSaveState;
 import android.util.Log;
+import android.os.SystemProperties;
 
 import androidx.annotation.Nullable;
 
+import com.android.internal.R;
+import com.android.internal.util.rebellion.Utils;
 import com.android.internal.annotations.VisibleForTesting;
 import com.android.settingslib.fuelgauge.BatterySaverUtils;
 import com.android.settingslib.fuelgauge.Estimate;
@@ -53,6 +57,7 @@ public class BatteryControllerImpl extends BroadcastReceiver implements BatteryC
     private static final String TAG = "BatteryController";
 
     public static final String ACTION_LEVEL_TEST = "com.android.systemui.BATTERY_LEVEL_TEST";
+    public static final String SPECTRUM_SYSTEM_PROPERTY = "persist.spectrum.profile";
 
     private static final boolean DEBUG = Log.isLoggable(TAG, Log.DEBUG);
     private static final int UPDATE_GRANULARITY_MSEC = 1000 * 60;
@@ -115,6 +120,16 @@ public class BatteryControllerImpl extends BroadcastReceiver implements BatteryC
     @Override
     public void setPowerSaveMode(boolean powerSave) {
         BatterySaverUtils.setPowerSaveMode(mContext, powerSave, /*needFirstTimeWarning*/ true);
+    }
+
+
+    private void deviceSpecificPerfermanceModeHandler(boolean enabled) {
+        if(enabled && Utils.IntelligentPerformanceProfileAvailable()) {
+            Utils.SetLastPerformanceProfileToSettings(mContext, SystemProperties.get(SPECTRUM_SYSTEM_PROPERTY, Resources.getSystem().getString(R.string.config_balanced_profile)));
+            SystemProperties.set(SPECTRUM_SYSTEM_PROPERTY, Resources.getSystem().getString(R.string.config_powersave_profile));
+        } else {
+            SystemProperties.set(SPECTRUM_SYSTEM_PROPERTY, Utils.GetLastPerformanceProfileFromSettings(mContext));
+        }
     }
 
     @Override
@@ -273,6 +288,12 @@ public class BatteryControllerImpl extends BroadcastReceiver implements BatteryC
     private void setPowerSave(boolean powerSave) {
         if (powerSave == mPowerSave) return;
         mPowerSave = powerSave;
+
+        if(powerSave) {
+            deviceSpecificPerfermanceModeHandler(true);
+        } else {
+            deviceSpecificPerfermanceModeHandler(false);
+        }
 
         // AOD power saving setting might be different from PowerManager power saving mode.
         PowerSaveState state = mPowerManager.getPowerSaveState(PowerManager.ServiceType.AOD);