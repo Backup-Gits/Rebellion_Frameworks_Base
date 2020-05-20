SuperiorOS/android_frameworks_base

Gestures nav: Add Skip/Previous Song Function [1/3]
DU: use our statusbar media actions implementation to have also remote spotify support,
and to be able to send different media actions from any class with the proper keycode

Change-Id: I9ef673b9c684c3754d4a0e99340de3023367f6b0
Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
@@ -712,7 +712,13 @@ public class EdgeBackGestureHandler implements DisplayListener {
                 SuperiorUtils.toggleRingerModes(mContext);
                 break;
             case 13: // Kill app
-                ActionUtils.killForegroundApp();
+                SuperiorUtils.killForegroundApp();
+                break;
+            case 14: // Skip song
+                SuperiorUtils.sendSystemKeyToStatusBar(KeyEvent.KEYCODE_MEDIA_NEXT);
+                break;
+            case 15: // Previous song
+                SuperiorUtils.sendSystemKeyToStatusBar(KeyEvent.KEYCODE_MEDIA_PREVIOUS);
                 break;
         }
     }

Backup-Gits/Rebellion_Frameworks_Base

Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/phone/EdgeBackGestureHandler.java
@@ -684,6 +684,12 @@ public class EdgeBackGestureHandler implements DisplayListener {
             case 13: // Kill app
                 ActionUtils.killForegroundApp();
                 break;
+            case 14: // Skip song
+                ActionUtils.sendSystemKeyToStatusBar(KeyEvent.KEYCODE_MEDIA_NEXT);
+                break;
+            case 15: // Previous song
+                ActionUtils.sendSystemKeyToStatusBar(KeyEvent.KEYCODE_MEDIA_PREVIOUS);
+                break;
         }
     }
 